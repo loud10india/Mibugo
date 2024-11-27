fc0dd0637b681245cccd8d27db92db6dd87b8ddd
Pooja Verma <poojaverma140400@gmail.com>
2024-11-27 15:03:58 +0530

    content change done

--- data/contact.js
+++ data/contact.js
@@ -1,31 +1,31 @@
 export const contactItems = [
-  {
-    iconClass: "mi-location",
-    title: "Address",
-    text: "Envato Pty Ltd, PO Box 16122 Collins Street, West Victoria.",
-    link: {
-      url: "https://goo.gl/maps/7Vw1PBhxx5BaXStr8",
-      text: "See Map",
-      rel: "nofollow noopener",
-      target: "_blank",
+    {
+        iconClass: "mi-location",
+        title: "Address",
+        text: "AltF Coworking, Suncity Success Tower, Sector 65, Gurugram, Haryana 122005",
+        link: {
+            url: "https://maps.app.goo.gl/A2EQeywmasGrb3r57",
+            text: "See Map",
+            rel: "nofollow noopener",
+            target: "_blank",
+        },
     },
-  },
-  {
-    iconClass: "mi-email",
-    title: "Email",
-    text: "ibthemes21@gmail.com",
-    link: {
-      url: "mailto:ibthemes21@gmail.com",
-      text: "Say Hello",
+    {
+        iconClass: "mi-email",
+        title: "Email",
+        text: "info@mibugo.com",
+        link: {
+            url: "mailto:ibthemes21@gmail.com",
+            text: "Say Hello",
+        },
     },
-  },
-  {
-    iconClass: "mi-mobile",
-    title: "Phone",
-    text: "+1 837 652 8800",
-    link: {
-      url: "tel:+61383767284",
-      text: "Call now",
+    {
+        iconClass: "mi-mobile",
+        title: "Phone",
+        text: "+91 88607 60700",
+        link: {
+            url: "tel:+91 88607 60700",
+            text: "Call now",
+        },
     },
-  },
 ];
