50db347d73b480a859051a76c30ba2188fd3fb74
poojavermaa <poojaverma140400@gmail.com>
2025-02-12 23:58:32 +0530

    fix : all issue

--- components/headers/Header7.jsx
+++ components/headers/Header7.jsx
@@ -37,7 +37,7 @@ export default function Header7({ links }) {
 
                 <ul className='items-end clearlist local-scroll'>
                     <li>
-                        <Link to='/contact' className='opacity-1 no-hover'>
+                        <Link href='/contact' className='opacity-1 no-hover'>
                             <span className='btn btn-mod btn-border-grad btn-small btn-round'>
                                 <span data-btn-animate='y'>Get in touch</span>
                             </span>
